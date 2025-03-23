fb78aa4cebcd340dd4fafd8abaaed597311d097a
pranav <pranavkalal36@gmail.com>
2025-03-23 13:33:29 +1000

    wer

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,10 @@
 
 const express = require('express');
-const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.route('/').get(protect, getTasks).post(protect, addTask);
+router.route('/').get(protect, getTasks).post(protect, createTask);
 router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 
+require("dotenv").config();
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
